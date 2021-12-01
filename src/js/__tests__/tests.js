1b6b8206eaeeb88ea10fb035b62eec5497f611e3
Konstantin Beloglazov <ajoq@ya.ru>
2021-12-01 11:34:00 +0300

    done v1.0.1

---
+++ src/js/__tests__/tests.js
@@ -0,0 +1,26 @@
+import Validator from '../app';
+
+test('only latin characters', () => {
+  const player = new Validator('Василий').validateUsername();
+  expect(player).toBe(false);
+});
+
+test.each([
+  ['Василий', false],
+  ['ajБoq', false],
+  ['我的天使', false],
+  ['ajoq-ajoq', true],
+  ['ajoq_ajoq', true],
+  ['ajoq123aj12oq', true],
+  ['ajoq12345aj1234oq', false],
+  ['1ajoq', false],
+  ['ajoq1', false],
+  ['_ajoq', false],
+  ['ajoq_', false],
+  ['-ajoq', false],
+  ['ajoq-', false],
+  ['aj$oq', false],
+])('regexp', (obj, expected) => {
+  const result = new Validator(obj).validateUsername();
+  expect(result).toBe(expected);
+});
