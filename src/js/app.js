b75f84446ddccf3e128cfa1437beb607aac9d79a
Konstantin Beloglazov <ajoq@ya.ru>
2021-12-01 11:33:50 +0300

    done v1.0.0

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,9 @@
-class Validator {
-    constructor(nickname) {
-        this.nickname = nickname;
-    }
+export default class Validator {
+  constructor(nickname) {
+    this.nickname = nickname;
+  }
 
-    validateUsername() {
-        return (/^[^\d-_][\w-]+[^\d-_]$/.test(this.nickname)) && !(/\d{4,}/.test(this.nickname));
-    }
+  validateUsername() {
+    return (/^[^\d-_][\w-]+[^\d-_]$/.test(this.nickname)) && !(/\d{4,}/.test(this.nickname));
+  }
 }
-
-const player1 = new Validator('ajoqo123f324f');
-console.log(player1.validateUsername());
-
-//Написать тесты к заданию
